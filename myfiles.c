ed2003ceb80329de46e8bdfca9a6866abcb20484
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-02-28 18:22:16 -0500

    Update myfiles.c

--- myfiles.c
+++ myfiles.c
@@ -1,36 +1,14 @@
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdio.h>
-#include <unistd.h>
 
 FILE *f1, *f2, *fout, *fin;
-char *f1Name, *f2Name, *foutName;
+const char *f1Name, *f2Name, *foutName;
 char ch;
-
-int main (int argc, char *argv[])
-{
-
-    if (argc < 2) {
-        printf("Usage: ./myfiles file_in_1 file_in_2 [file_out (default:myfile.out)]\n");
-        return(1);
-    }
-
-    else {
-        f1 = argv[1];
-        f2 = argv[2];
-        fout = argv[3];
-    }
-
-    CopyFile(fileno(fout), f1);
-
-    return 0;
-}
 
 void WriteInFile (int fd, const char *buff, int len)
 {
-    fout = fopen(fout ,"w");
+    fout = fopen(foutName ,"w");
 
-    while ( (ch = fgetc(fd)) != EOF ) {
+    while ( (ch = fgetc(f1)) != EOF ) {
         fputc(ch, fout);
     }
 
@@ -42,5 +20,24 @@ void CopyFile (int fd, const char *file_in)
     fout = fdopen(fd ,"w");
     fin = fopen(file_in ,"r");
 
-    WriteInFile(fd, *file_in, 1);
+    WriteInFile(fd, file_in, 1);
+}
+
+int main (int argc, char *argv[])
+{
+
+    if (argc < 2) {
+        printf("Usage: ./myfiles file_in_1 file_in_2 [file_out (default:myfile.out)]\n");
+        return(1);
+    }
+
+    else {
+        f1Name = argv[1];
+        f2Name = argv[2];
+        foutName = argv[3];
+    }
+
+    CopyFile(fileno(fout), f1Name);
+
+    return 0;
 }
