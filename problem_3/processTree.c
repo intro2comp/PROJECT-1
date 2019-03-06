91b1f0470ea4cea1710a39363b826d75f6233246
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2019-03-06 12:21:14 -0500

    Add Problem 3
    unfinished

---
+++ problem_3/processTree.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid, pid1, pid2, pid3;
+
+    pid = fork();
+
+    if (pid == 0) {
+        pid1 = fork();
+
+        if (pid1 == 0) {
+            sleep(5);
+            printf("D\n");
+            return 10;
+        }
+
+        else {
+            wait(NULL);
+            printf("B\n");
+            return 4;
+        }
+    }
+
+    else {
+        wait(NULL);
+        printf("C\n");
+        return 6;
+    }
+}
