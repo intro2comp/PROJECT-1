33e8d959d2af14801c8640930334128538c541ce
Amandip Kaler <43186615+ak1415@users.noreply.github.com>
2019-03-09 05:42:33 -0500

    Update prob2part1.c

--- problem_2/prob2part1.c
+++ problem_2/prob2part1.c
@@ -1,51 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-int shmid;
-int *numbers;
-int size;
 
-int main(int argc, char *argv[]){
-  struct timeval start,end;
-  gettimeofday(&start,NULL);
+int *integers, size, sharedmem;
 
-  shmid = shmget(IPC_PRIVATE, 1000000*sizeof(int), 0666 | IPC_CREAT);
-  numbers = shmat(shmid, 0, 0);
 
-  FILE *input = fopen(argv[1], "r");
+int main(int argc, char *a[]){
+    struct timeval start,end;
+    gettimeofday(&start,NULL);
 
-  size = 0;
-  while(fscanf(input, "%d\n", &numbers[size++]) != EOF);
+    sharedmem = shmget(IPC_PRIVATE, 1000000*sizeof(int), 0666 | IPC_CREAT);
 
-  int count = 0;
-  int sum = 0;
-  int max = INT_MIN;
-  int min = INT_MAX;
-  while(count < size){
-    int readn = numbers[count];
-    if(readn < min) min = readn;
-    if(readn > max) max = readn;
-    sum += readn;
-    count++;
-  }
+    integers = shmat(sharedmem, 0, 0);
 
-  FILE *out = fopen(argv[2], "w+");
-  fprintf(out, "hi, i\'m process %d and my parent is %d\n", getpid(), getppid());
-  fprintf(out, "min: %d\n", min);
-  fprintf(out, "max: %d\n", max);
-  fprintf(out, "sum: %d\n", sum);
+    FILE *input = fopen(a[1], "r");
 
-  shmdt(numbers);
-  shmctl(shmid, IPC_RMID, NULL);
+    size = 0;
+    while(fscanf(input, "%d\n", &integers[size++]) != EOF);
 
-  gettimeofday(&end,NULL);
-  printf("%ld microseconds\n", (end.tv_sec*100000 + end.tv_usec) - (start.tv_sec*100000 + start.tv_usec));
+    int iteration = 0;
+    int sum = 0;
+    int min = INT_MAX;
+    int max = INT_MIN;
+    while(iteration < size){
+        int readin = integers[iteration];
+        if(readin < min) min = readin;
+        if(readin > max) max = readin;
+        sum += readin;
+        iteration++;
+    }
 
-  return 0;
+
+    FILE *out = fopen(a[2], "w+");
+    fprintf(out, "Hi I\'m process %d and my parent is %d.\n", getpid(), getppid());
+    fprintf(out, "Min: %d\n", min);
+    fprintf(out, "Max: %d\n", max);
+    fprintf(out, "Sum: %d\n", sum);
+
+    shmdt(integers);
+    shmctl(sharedmem, IPC_RMID, NULL);
+
+    gettimeofday(&end,NULL);
+    printf("%ld microseconds\n", (end.tv_sec*100000 + end.tv_usec) - (start.tv_sec*100000 + start.tv_usec));
+
+    return 0;
 }
