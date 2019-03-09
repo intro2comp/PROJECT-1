aec2ef45d1ef4f964fb51ebded457ec1a093648c
Amandip Kaler <43186615+ak1415@users.noreply.github.com>
2019-03-09 05:57:38 -0500

    Update prob2part1.c

--- problem_2/prob2part1.c
+++ problem_2/prob2part1.c
@@ -1,50 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-
-int *integers, size, sharedmem;
-
+int shmid;
+int *numbers;
+int size;
 
 int main(int argc, char *argv[]){
-    struct timeval start,end;
-    gettimeofday(&start,NULL);
+  struct timeval start,end;
+  gettimeofday(&start,NULL);
 
-    sharedmem = shmget(IPC_PRIVATE, 1000000*sizeof(int), 0666 | IPC_CREAT);
+  shmid = shmget(IPC_PRIVATE, 1000000*sizeof(int), 0666 | IPC_CREAT);
+  numbers = shmat(shmid, 0, 0);
 
-    integers = shmat(sharedmem, 0, 0);
+  FILE *input = fopen(argv[1], "r");
 
-    FILE *input = fopen(argv[1], "r");
+  size = 0;
+  while(fscanf(input, "%d\n", &numbers[size++]) != EOF);
 
-    size = 0;
-    while(fscanf(input, "%d\n", &integers[size++]) != EOF);
+  int count = 0;
+  int sum = 0;
+  int max = INT_MIN;
+  int min = INT_MAX;
+  while(count < size){
+    int readn = numbers[count];
+    if(readn < min) min = readn;
+    if(readn > max) max = readn;
+    sum += readn;
+    count++;
+  }
 
-    int iteration = 0;
-    int sum = 0;
-    int min = INT_MAX;
-    int max = INT_MIN;
-    while(iteration < size){
-        int readin = integers[iteration];
-        if(readin < min) min = readin;
-        if(readin > max) max = readin;
-        sum += readin;
-        iteration++;
-    }
+  FILE *out = fopen(argv[2], "w+");
+  fprintf(out, "hi, i\'m process %d and my parent is %d\n", getpid(), getppid());
+  fprintf(out, "min: %d\n", min);
+  fprintf(out, "max: %d\n", max);
+  fprintf(out, "sum: %d\n", sum);
 
+  shmdt(numbers);
+  shmctl(shmid, IPC_RMID, NULL);
 
-    FILE *out = fopen(argv[2], "w+");
-    fprintf(out, "Hi I\'m process %d and my parent is %d\n", getpid(), getppid());
-    fprintf(out, "Min: %d\n", min);
-    fprintf(out, "Max: %d\n", max);
-    fprintf(out, "Sum: %d\n", sum);
+  gettimeofday(&end,NULL);
+  printf("%ld microseconds\n", (end.tv_sec*100000 + end.tv_usec) - (start.tv_sec*100000 + start.tv_usec));
 
-    shmdt(integers);
-    shmctl(sharedmem, IPC_RMID, NULL);
-
-    gettimeofday(&end,NULL);
-    printf("%ld microseconds\n", (end.tv_sec*100000 + end.tv_usec) - (start.tv_sec*100000 + start.tv_usec));
-
-    return 0;
+  return 0;
 }
