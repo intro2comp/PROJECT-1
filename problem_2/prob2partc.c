2c6b9b2593bfeb5aa3255a67db117d65fdc512e7
Amandip Kaler <43186615+ak1415@users.noreply.github.com>
2019-03-09 06:59:26 -0500

    Update prob2partc.c

--- problem_2/prob2partc.c
+++ problem_2/prob2partc.c
@@ -7,16 +7,18 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-FILE *writeFile;
-int shmid;
-int *numbers;
-int size;
-int perProcess;
 
-void process(int start, info_struct *info, int perProcessRemainder){
-  if (start >= size) return;
-  int end = start+perProcess;
-  if (perProcessRemainder > 0) end++;
+
+FILE *writeFile;
+
+
+int *integers, sharedmem, size, num;
+
+
+void process(int begin, info_struct *data, int numRemainder){
+  if (begin >= size) return;
+  int end = begin+num;
+  if (numRemainder > 0) end++;
 
   int fd[2];
   pipe(fd);
@@ -26,32 +28,32 @@ void process(int start, info_struct *info, int perProcessRemainder){
   childpid = fork();
 
   if (childpid == 0){
-    fprintf(writeFile, "hi, i\'m process %d and my parent is %d\n", getpid(), getppid());
+    fprintf(writeFile, "Hi I\'m process %d and my parent is %d.\n", getpid(), getppid());
     close(fd[0]);
 
-    while(start < end){
-      int number = numbers[start];
-      if (number < info->min) info->min = number;
-      if (number > info->max) info->max = number;
-      info->sum += number;
-      info->count++;
-      start++;
+    while(begin < end){
+      int number = integers[begin];
+      if (number < data->min) data->min = number;
+      if (number > data->max) data->max = number;
+      data->sum += number;
+      data->count++;
+      begin++;
     }
 
-    write(fd[1], info, sizeof(info_struct));
+    write(fd[1], data, sizeof(info_struct));
     exit(0);
   }
   else{
     close(fd[1]);
 
-    process(end, info, perProcessRemainder-1);
+    process(end, data, numRemainder-1);
 
     info_struct new_info;
     read(fd[0], &new_info, sizeof(info_struct));
-    if (new_info.min < info->min) info->min = new_info.min;
-    if (new_info.max > info->max) info->max = new_info.max;
-    info->sum += new_info.sum;
-    info->count += new_info.count;
+    if (new_info.min < data->min) data->min = new_info.min;
+    if (new_info.max > data->max) data->max = new_info.max;
+    data->sum += new_info.sum;
+    data->count += new_info.count;
 
     waitpid(childpid);
   }
@@ -61,16 +63,16 @@ int main(int argc, char *argv[]){
   struct timeval start,end;
   gettimeofday(&start,NULL);
 
-  shmid = shmget(IPC_PRIVATE, 1000000*sizeof(int), 0666 | IPC_CREAT);
-  numbers = shmat(shmid, 0, 0);
+  sharedmem = shmget(IPC_PRIVATE, 1000000*sizeof(int), 0666 | IPC_CREAT);
+  integers = shmat(sharedmem, 0, 0);
 
   FILE *readFile = fopen(argv[1], "r");
   writeFile = fopen(argv[2], "w+");
   int numProcesses = atoi(argv[3]);
 
   size = 0;
-  while(fscanf(readFile, "%d\n", &numbers[size++]) != EOF);
-  perProcess = size/numProcesses;
+  while(fscanf(readFile, "%d\n", &integers[size++]) != EOF);
+  num = size/numProcesses;
   int perProcessRemainder = size%numProcesses;
 
   info_struct info = {.min = INT_MAX, .max = INT_MIN, .sum = 0, .count = 0};
@@ -83,8 +85,8 @@ int main(int argc, char *argv[]){
   fclose(readFile);
   fclose(writeFile);
 
-  shmdt(numbers);
-  shmctl(shmid, IPC_RMID, NULL);
+  shmdt(integers);
+  shmctl(sharedmem, IPC_RMID, NULL);
 
   gettimeofday(&end,NULL);
   printf("%ld microseconds\n", (end.tv_sec*100000 + end.tv_usec) - (start.tv_sec*100000 + start.tv_usec));
