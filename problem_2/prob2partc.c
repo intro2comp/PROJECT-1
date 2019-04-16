ca69dcd75a4d9dcbd66fa118fa3025a78542c5dc
andrewsengupta <43046111+andrewsengupta@users.noreply.github.com>
2019-04-15 21:44:08 -0400

    Update prob2partc.c

--- problem_2/prob2partc.c
+++ problem_2/prob2partc.c
@@ -1,92 +1,157 @@
-#include "data.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <string.h>
 
+typedef enum { false, true } bool;
 
+union sigval cs;
+int currentValue;
 
-FILE *writeFile;
-
-
-int *integers, sharedmem, size, num;
-
-
-void process(int begin, info_struct *data, int numRemainder){
-  if (begin >= size) return;
-  int end = begin+num;
-  if (numRemainder > 0) end++;
-
-  int fd[2];
-  pipe(fd);
-  pid_t childpid;
-
-  fflush(writeFile);
-  childpid = fork();
-
-  if (childpid == 0){
-    fprintf(writeFile, "Hi I\'m process %d and my parent is %d.\n", getpid(), getppid());
-    close(fd[0]);
-
-    while(begin < end){
-      int number = integers[begin];
-      if (number < data->min) data->min = number;
-      if (number > data->max) data->max = number;
-      data->sum += number;
-      data->count++;
-      begin++;
-    }
-
-    write(fd[1], data, sizeof(info_struct));
-    exit(0);
-  }
-  else{
-    close(fd[1]);
-
-    process(end, data, numRemainder-1);
-
-    info_struct new_info;
-    read(fd[0], &new_info, sizeof(info_struct));
-    if (new_info.min < data->min) data->min = new_info.min;
-    if (new_info.max > data->max) data->max = new_info.max;
-    data->sum += new_info.sum;
-    data->count += new_info.count;
-
-    waitpid(childpid);
-  }
+char* createMinMaxSumString(int minimum, int maximum, int sum){
+    char* str  = malloc(sizeof(char) * 100);
+    sprintf(str, "%d %d %d", minimum, maximum, sum);
+    return str;
 }
 
-int main(int argc, char *argv[]){
-  struct timeval start,end;
-  gettimeofday(&start,NULL);
+char* createNewMinMaxSumString(char* oldString, int currVal){
+    int currentMinimum= 0 , currentMaximum = 0, currentSum= 0;
+    sscanf(oldString, "%d %d %d", &currentMinimum, &currentMaximum, &currentSum);
+    
+    if(currentValue > currentMaximum){
+        currentMaximum = currentValue;
+    }
+    
+    if(currentValue < currentMinimum){
+        currentMinimum = currentValue;
+    }
+    
+    currentSum = currentSum + currentValue;
+    return createMinMaxSumString(currentMinimum, currentMaximum, currentSum);
+}
 
-  sharedmem = shmget(IPC_PRIVATE, 1000000*sizeof(int), 0666 | IPC_CREAT);
-  integers = shmat(sharedmem, 0, 0);
 
-  FILE *readFile = fopen(argv[1], "r");
-  writeFile = fopen(argv[2], "w+");
-  int numProcesses = atoi(argv[3]);
+void sig_action_function(int sig, siginfo_t *info, void *ptr)
+{
+    cs = info->si_value;
+    cs.sival_ptr =  createNewMinMaxSumString(cs.sival_ptr, currentValue);
+    printf("Signal: %d Sum: %s\n", info->si_pid, (char*) cs.sival_ptr);
+}
 
-  size = 0;
-  while(fscanf(readFile, "%d\n", &integers[size++]) != EOF);
-  num = size/numProcesses;
-  int perProcessRemainder = size%numProcesses;
 
-  info_struct info = {.min = INT_MAX, .max = INT_MIN, .sum = 0, .count = 0};
-  process(0, &info, perProcessRemainder);
+typedef struct Struct{
+    int* integerArray;
+    int arraySize;
+} Struct;
 
-  fprintf(writeFile, "min: %d\n", info.min);
-  fprintf(writeFile, "max: %d\n", info.max);
-  fprintf(writeFile, "sum: %d\n", info.sum);
+const int DEBUG = 0;
 
-  fclose(readFile);
-  fclose(writeFile);
+int parseFile(char* filePath){
+    FILE* file = fopen(filePath, "r");
+    int fd = fileno(file);
+    char buffer;
+    while(read(fd, &buffer, 1) != 0){
+        printf("%c", buffer);
+    }
+    
+    fclose (file);
+    return 0;
+}
 
-  shmdt(integers);
-  shmctl(sharedmem, IPC_RMID, NULL);
+Struct* parseIntFile(char* filePath){
+    FILE* file = fopen(filePath, "r");
+    int fd = fileno(file);
+    char buffer;
+    int count = 0;
+    while(read(fd, &buffer, 1) != 0){
+        if(buffer == '\n'){
+            count++;
+        }
+    }
+    
+    fclose(file);
+    
+    int* nums = malloc(sizeof(int) * count);
+    
+    file = fopen(filePath, "r");
+    
+    int num = 0;
+    int currentCount = 0;;
+    while(!feof(file) && (currentCount < count)){
+        fscanf (file, "%d", &num);
+        nums[currentCount] = num;
+        currentCount++;
+    }
+    
+    fclose(file);
+    
+    Struct *ret = malloc(sizeof(Struct));
+    
+    ret->integerArray = nums;
+    ret->arraySize = count;
+    
+    
+    return ret;
+}
 
-  gettimeofday(&end,NULL);
-  printf("%ld microseconds\n", (end.tv_sec*100000 + end.tv_usec) - (start.tv_sec*100000 + start.tv_usec));
+int getMaximum(Struct* nums){
+    int ret = INT_MIN;
+    
+    int i;
+    for(i = 0; i < nums->arraySize; i++){
+        if(nums->integerArray[i] > ret){
+            ret = nums->integerArray[i];
+        }
+    }
+    
+    return ret;
+}
+
+int getMinimum(Struct* nums){
+    int ret = INT_MAX;
+    
+    int i;
+    for(i = 0; i < nums->arraySize; i++){
+        if(nums->integerArray[i] < ret){
+            ret = nums->integerArray[i];
+        }
+    }
+    
+    return ret;
+}
+
+int getSum(Struct* nums){
+    int ret = 0;
+    
+    int i;
+    for(i = 0; i < nums->arraySize; i++){
+        ret += nums->integerArray[i];
+    }
+    
+    return ret;
+}
+
+void printSum(char* dataString){
+    int currentMinimum= 0 , currentMaximum = 0, currentSum= 0;
+    sscanf(dataString, "%d %d %d", &currentMinimum, &currentMaximum, &currentSum);
+    printf("Max=%d\n", currentMaximum);
+    printf("Min=%d\n", currentMinimum);
+    printf("Sum=%d\n", currentSum);
+    return;
+}
+
+int multiProcessor(Struct* nums, int ind){
+    pid_t pid;
+    printf("hi, I am process %d and my parent is %d.\n", getpid(), getppid());
+    int stat;
+    pid = fork();
+    if(pid == 0){
+        currentValue = nums->integerArray[ind];
+        if(ind == nums->arraySize - 1){
+            if(DEBUG) { printf("%d at index %d FINAL CHILD\n", nums->integerArray[ind], ind);}
+            cs.sival_ptr = createMinMaxSumString(currentValue, currentValue, currentValue);
+            
